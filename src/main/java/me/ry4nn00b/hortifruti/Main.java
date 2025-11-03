bfcf91216aec59c1c95bea81f1174496b8cc9c87
Ry4nN00B <ryangomesxaviersociais@gmail.com>
2025-11-02 21:13:58 -0300

    Update and Implement new Code

--- src/main/java/me/ry4nn00b/hortifruti/Main.java
+++ src/main/java/me/ry4nn00b/hortifruti/Main.java
@@ -3,7 +3,6 @@ package me.ry4nn00b.hortifruti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableScheduling
